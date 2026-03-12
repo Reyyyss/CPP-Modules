d61f135a4b387e2f51b4ef74192686e8c2b1ef54
Reyyyss <hcr2207@gmail.com>
2026-03-12 18:17:05 +0000

    almost finishig exercise 01

--- cpp00/ex01/Phoneboo.cpp
+++ cpp00/ex01/Phoneboo.cpp
@@ -6,7 +6,7 @@
 /*   By: hcarrasq <hcarrasq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/12 11:15:00 by hcarrasq          #+#    #+#             */
-/*   Updated: 2026/03/12 12:16:01 by hcarrasq         ###   ########.fr       */
+/*   Updated: 2026/03/12 18:15:03 by hcarrasq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,11 +33,44 @@ static std::string formating_field(const std::string &field)
 	else
 		return std::string(width - field.size(), ' ') + field;
 }
+static bool read_contact_inputs(const std::string &prompt, std::string &out)
+{
+	while (1)
+	{
+		std::cout << prompt;
+		if (!std::getline(std::cin, out));
+			return (false);
+		trim_input(out);
+		if (!out.empty())
+			return (true);
+		std::cout << "This field can't be empty" << std::endl;
+	}
+}
 
 void PhoneBook::add()
 {
-	while(1)
-	{
-		std::string input;
+	std::string first, last, nick, secret, phone;
+	if (!read_contact_inputs("First Name: ", first)) { std::cout << "Error adding First Name";
+		return ;
 	}
+	if (!read_contact_inputs("Last Name: ", last)) { std::cout << "Error adding Last Name";
+		return ;
+	}
+	if (!read_contact_inputs("NickName: ", nick)) { std::cout << "Error adding NickName";
+		return ;
+	}
+	if (!read_contact_inputs("Secret: ", secret)) { std::cout << "Error adding Secret";
+		return ;
+	}
+	if (!read_contact_inputs("First Name: ", first)) { std::cout << "Error adding Phone Number";
+		return ;
+	}
+	
+	Contact c;
+	
+	c.setDarkestSecret(secret);
+	c.setFirstName(first);
+	c.setLastName(last);
+	c.setNickName(nick);
+	c.setPhoneNumber(phone);
 }
