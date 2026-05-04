8f7c91d5320c0e7b12855198c399f8804fb2bd97
Henrique <hcr2207@gmail.com>
2026-05-04 10:22:16 +0000

    cpp01 finished

--- cpp01/ex05/notsed.cpp
+++ cpp01/ex05/notsed.cpp
@@ -11,9 +11,10 @@
 /* ************************************************************************** */
 
 #include "notsed.hpp"
+#include <iterator>
 
 NotSed::NotSed(const std::string &replacement_file, const std::string &replaced, const std::string &replacement)
-	: input_file(replacement_file),  search_str(replaced), replace_str(replacement)
+	: search_str(replaced), replace_str(replacement), input_file(replacement_file)
 {
 		
 }
@@ -26,11 +27,28 @@ NotSed::~NotSed()
 void	NotSed::Replacer()
 {
 	std::ifstream in_file(input_file.c_str(), std::ios::in | std::ios::binary);
-	if (!in_file){
+	if (!in_file)
+	{
 		std::cout << "Error trying to create the input file stream" << std::endl;
 		return ;
 	}
 	std::string file_str;
-	file_str.assign(std::istreambuf_iterator<char>(in), std::istreambuf_iterator<char>());
+	file_str.assign(std::istreambuf_iterator<char>(in_file), std::istreambuf_iterator<char>());
 	in_file.close();	
+	size_t	pos = 0;
+	while ((pos = file_str.find(search_str, pos)) != std::string::npos)
+	{
+		file_str.erase(pos, search_str.length());
+		file_str.insert(pos, replace_str);
+		pos += replace_str.length();
+	}
+	std::string output_file = input_file + ".replaced";
+	std::ofstream out(output_file.c_str(), std::ios::binary | std::ios::trunc);
+	if (!out)
+	{
+		std::cout << "Error creating the output file" << std::endl;
+		return ;
+	}
+	out << file_str;
+	out.close();
 }
